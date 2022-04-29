c35a6a4a3d0fa6571b8223deb2f9a5d0b3bc82ac
PyMoDanny <meisterloewe@gmail.com>
2022-04-29 13:11:26 +0200

    Something more

--- demo_app_py/main.py
+++ demo_app_py/main.py
@@ -1,12 +1,21 @@
-min_to_sec = 60
+min_to_sec = 2 * 5 * 6
 unit_of_measure = "seconds"
 print(3 * min_to_sec)
 
 
-def minute_in_seconds(number_of_minutes, add_comment):
-    print(f"Calculating {number_of_minutes} * {min_to_sec} -> {number_of_minutes * min_to_sec} {unit_of_measure} {add_comment}")
+def minute_in_seconds(number_of_minutes):
+    print(number_of_minutes > 0)
+    if number_of_minutes > 0:
+        return f"Calculating {number_of_minutes} * {min_to_sec} -> {number_of_minutes * min_to_sec} {unit_of_measure}"
+    else:
+        return("No Dice!")
 
+#user_input = "b"
+#minute_in_seconds("a")
+#minute_in_seconds(user_input)
 
-minute_in_seconds("a", "Hallo Welt")
-minute_in_seconds(5, "Hallo Welt")
-minute_in_seconds(7, "Hallo Welt")
+user_input = input("Give me input:\n")
+user_input_number = int(user_input)
+result = minute_in_seconds(user_input_number)
+print(result)
+
