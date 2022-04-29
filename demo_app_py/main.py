f54d738c41b4c669eb299b216daad581c6c563ed
PyMoDanny <meisterloewe@gmail.com>
2022-04-29 10:53:42 +0200

    Cleanup

--- demo_app_py/main.py
+++ demo_app_py/main.py
@@ -3,10 +3,10 @@ unit_of_measure = "seconds"
 print(3 * min_to_sec)
 
 
-def minute_in_seconds(user_input):
-    print(f"Calculating {user_input} * {min_to_sec} -> {user_input * min_to_sec} {unit_of_measure}")
+def minute_in_seconds(number_of_minutes, add_comment):
+    print(f"Calculating {number_of_minutes} * {min_to_sec} -> {number_of_minutes * min_to_sec} {unit_of_measure} {add_comment}")
 
 
-minute_in_seconds(3)
-minute_in_seconds(5)
-minute_in_seconds(7)
+minute_in_seconds("a", "Hallo Welt")
+minute_in_seconds(5, "Hallo Welt")
+minute_in_seconds(7, "Hallo Welt")
