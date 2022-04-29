acd5dc732206ac52ab9a5c824a38bb0d720faeb4
PyMoDanny <meisterloewe@gmail.com>
2022-04-29 10:40:32 +0200

    Clanup

--- demo_app_py/main.py
+++ demo_app_py/main.py
@@ -1,6 +1,12 @@
-transform = 100
+min_to_sec = 60
 unit_of_measure = "seconds"
-eingabe = 5
-print(3 * transform)
+print(3 * min_to_sec)
 
-print(f"Calculating {eingabe} * {transform} -> {eingabe * transform} {unit_of_measure}")
+
+def minute_in_seconds(user_input):
+    print(f"Calculating {user_input} * {min_to_sec} -> {user_input * min_to_sec} {unit_of_measure}")
+
+
+minute_in_seconds(3)
+minute_in_seconds(5)
+minute_in_seconds(7)
