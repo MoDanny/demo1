18ffefb66956914accb53865d9bc025f019ab9b4
PyMoDanny <meisterloewe@gmail.com>
2022-04-29 13:44:16 +0200

    Something more

--- demo_app_py/main.py
+++ demo_app_py/main.py
@@ -4,18 +4,19 @@ print(3 * min_to_sec)
 
 
 def minute_in_seconds(number_of_minutes):
-    print(number_of_minutes > 0)
     if number_of_minutes > 0:
         return f"Calculating {number_of_minutes} * {min_to_sec} -> {number_of_minutes * min_to_sec} {unit_of_measure}"
-    else:
-        return("No Dice!")
+    elif number_of_minutes == 0:
+        return "No Zero allowed"
 
-#user_input = "b"
-#minute_in_seconds("a")
-#minute_in_seconds(user_input)
+def validate_and_execute():
+    if user_input.isdigit():
+        user_input_number = int(user_input)
+        result = minute_in_seconds(user_input_number)
+        print(result)
+    else:
+        print("meh!!!")
 
 user_input = input("Give me input:\n")
-user_input_number = int(user_input)
-result = minute_in_seconds(user_input_number)
-print(result)
+validate_and_execute()
 
