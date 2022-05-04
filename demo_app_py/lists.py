0214d8057ad914debf1f554f828ebad8bd92a703
PyMoDanny <meisterloewe@gmail.com>
2022-05-04 20:36:30 +0200

    Lists / Sets

--- demo_app_py/lists.py
+++ demo_app_py/lists.py
@@ -1,7 +1,7 @@
 demo_list = ["Omega", "abc", "xyz", "Omega"]
 element = demo_list[0]
 print(demo_list)
-print (element)
+print(element)
 print(demo_list[2])
 demo_list.append("Theta")
 demo_list.append("Epsilon")
@@ -11,28 +11,47 @@ print(demo_list)
 demo_list.remove("Omega")
 print(demo_list)
 
-print ("********************************************** Sets")
+print("********************************************** Sets")
 demo_set = {"abc", "xyz", "Omega"}
-print (demo_set)
+print(demo_set)
 
 for element in demo_set:
     print(element)
 
 demo_set.add("Salbei")
-print (demo_set)
+print(demo_set)
 try:
     demo_set.remove("Omega")
 except KeyError:
     print("notinlist")
-print (demo_set)
+print(demo_set)
 
 print("Hallo Welt".replace("Welt", "Elch"))
 
-
-print ("********************************************** Dictionary")
-demo_dict = {"days":10,
-             "hours":4,
+print("********************************************** Dictionary")
+demo_dict = {"days": 10,
+             "hours": 4,
              "Notb": 666}
 
 print(demo_dict)
 print(type(demo_dict))
+
+# Modules #
+timetable = {"hours": 24, "minutes": 1440, "seconds": 86400}
+user_input_message = "Give me days and unit. e.g. 45:hours\n"
+
+
+def days_to_units(num_of_days, conversion_unit):
+    try:
+        return f"{num_of_days} days are {num_of_days * timetable[conversion_unit]} {conversion_unit}"
+    except KeyError:
+        return "Doofus"
+
+
+def validate_and_execute(days_and_unit_dict):
+    try:
+        user_input_number = int(days_and_unit_dict["days"])
+        calculated_value = days_to_units(user_input_number, days_and_unit_dict["unit"])
+        print(calculated_value)
+    except ValueError:
+        print("Invalid Input")
--- demo_app_py/main.py
+++ demo_app_py/main.py
@@ -1,28 +1,11 @@
-timetable = {"hours": 24, "minutes": 1440, "seconds": 86400 }
-
-def days_to_units(num_of_days, conversion_unit):
-
-    try:
-        return f"{num_of_days} days are {num_of_days * timetable[conversion_unit]} {conversion_unit}"
-    except KeyError:
-        return "Doofus"
-
-def validate_and_execute():
-    try:
-        user_input_number = int(days_and_unit_dict["days"])
-        calculated_value = days_to_units(user_input_number, days_and_unit_dict["unit"])
-        print(calculated_value)
-    except ValueError:
-        print("Invalid Input")
+from lists import validate_and_execute, user_input_message
 
 
 user_input = ""
 while user_input != "exit":
-    user_input = input("Give me days and unit. e.g. 45:hours\n")
+    user_input = input(user_input_message)
     # set - liste mit diskreten elementen - list hat [] aber ein set hat {}
     # list hat also eine Reihenfolge, set nicht. set ist eine Menge.
     days_and_unit = user_input.split(":")
-    print(days_and_unit)
     days_and_unit_dict = {"days": days_and_unit[0], "unit": days_and_unit[1]}
-    print(days_and_unit_dict)
-    validate_and_execute()
+    validate_and_execute(days_and_unit_dict)
