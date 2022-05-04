dfb339cde29c3495db749785935f5faaf496f427
PyMoDanny <meisterloewe@gmail.com>
2022-05-04 09:52:05 +0200

    Lists

---
+++ demo_app_py/lists.py
@@ -0,0 +1,6 @@
+demo_list = ["abc", "xyz", "Omega"]
+element1 = demo_list[0]
+print (element1)
+print(demo_list[2])
+demo_list.append("Theta")
+print(demo_list)
--- demo_app_py/main.py
+++ demo_app_py/main.py
@@ -4,6 +4,8 @@ demo_list = ["abc", "xyz", "Omega"]
 element1 = demo_list[0]
 print (element1)
 print(demo_list[2])
+demo_list.append("Theta")
+print(demo_list)
 
 
 def days_to_units(num_of_days):
